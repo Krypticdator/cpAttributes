00c0a574e8d6d6ffd7e670ffb29affeff3448edc
Krypticdator <toni.nurmi@hotmail.com>
2016-03-09 13:08:51 +0200

    Added Setup.py

---
+++ Setup.py
@@ -0,0 +1,12 @@
+__author__ = 'Toni'
+
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='cpAttributes',
+      version='1.0',
+      description='cpAttributes to use for character',
+      author='Toni Nurmi',
+      author_email='toni.nurmi@hotmail.com',
+     )
